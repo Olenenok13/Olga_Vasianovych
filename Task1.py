595c8d4403af90f814c7f9ea2452d07966d3d59a
Olenenok13 <olga.vasianovych@gmail.com>
2021-01-12 22:32:16 +0200

    The task is done

---
+++ Task1.py
@@ -0,0 +1,10 @@
+Number1 = int(input("Введи первую цифру:"))
+Number2 = int(input("Введи вторую цифру:"))
+print("Арифметические операции с этими цифрами:\n"
+      "Сложение {} + {} = {} \n"
+      "Вычитание {} - {} = {} \n"
+      "Умножение {} * {} = {} \n"
+      "Деление {} / {} = {} \n".format(Number1, Number2, Number1 + Number2,
+                                       Number1, Number2, Number1 - Number2,
+                                       Number1, Number2, Number1 * Number2,
+                                       Number1, Number2, Number1 / Number2))
